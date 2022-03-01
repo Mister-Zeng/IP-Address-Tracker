01f54bd6f7e54f4feb844eb6a0fed3c7871fbe3d
Jason <92811609+Mister-Zeng@users.noreply.github.com>
2022-03-01 17:07:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,66 @@
-const map = L.map('map').setView([51.505, -0.09], 13);
+//api files
+const api_key = 'at_udqU3AVVicMujkURhDkkHciDgU47U';
+let url = 'https://geo.ipify.org/api/v2/';
+const bypass_cors_url = 'https://cors-anywhere.herokuapp.com/';
+
+// elements to update
+let currentIPAddress = document.querySelector('#IPAddressNum');
+let currentLocation = document.querySelector('#locationNum');
+let currentTimeZone = document.querySelector('#timeZoneNum');
+let currentISP = document.querySelector('#ISPNum');
+
+//form element
+let ip = document.querySelector('.searchIPAddress');
+const searchButton = document.querySelector('.arrow');
+
+const config = {
+    headers: { 'Access-Control-Allow-Origin': '*' }
+}
+
+var map = L.map('map').setView([40.7, -73.9], 12);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
 
-/*
-let IPAddress = document.querySelector('.IPAddressNum');
-let location = document.querySelector('.locationNum');
-let timeZone = document.querySelector('.timeZoneNum');
-let ISP = document.querySelector('.ISPNum');
-const search = document.querySelector('.arrow');
-let ip = "8.8.8.8";
-const api_key = "1548105";
 
-const fetchData = async () => {
-    const searchWord = document.querySelector('.searchIPAddress')
-    const config = { params: { ipAddress: searchWord, domain: searchWord, email: searchWord } }
-    const res = await fetch('https://geo.ipify.org/api/v2/', config);
-    const data = await res.json();
-    IPAddress.innerText = data.ip;
-    location.innerText = `(data.location.city), (data.location.region) (data.location.postalCode)`;
-    timeZone.innerText = data.location.timezone
-    ISP.innerText = data.isp;
-};
+updateMarker = (update_marker = [40.7, -73.9]) => {
+    map.setView(update_marker, 12)
+    L.marker(update_marker).addTo(map)
+}
+
+getIPDetails = (default_ip) => {
+    if (default_ip == undefined) {
+        let ip_url = `${url}?apikey=${api_key}`
+    } else {
+        let ip_url = `${url}?apikey=${api_key}&ipAddress=${default_ip} `
+    }
+    //fetch json data
+    const res = async () => {
+        try {
+            axios.get(ip_url, config)
+            const data = res.json()
+            currentIPAddress.innerHtml = data.ip;
+            currentLocation.innerHtml = `(data.location.city), (data.location.region)(data.location.postalCode)`;
+            currentTimeZone.innerHtml = data.location.timezone;
+            currentISP.innerHtml = data.isp;
+            updateMarker([data.location.lat, data.location.lng])
+        } catch (e) {
+            alert("Unable to get IP details")
+            console.log(error)
+        }
+    }
+}
+
+getIPDetails();
+
+document.addEventListener('load', updateMarker())
+
+searchButton.addEventListener('click', function (e) {
+    e.preventDefault
+    if (entered_ip.value != '' && eneter_ip.value != null) {
+        getIPDetails(entered_ip.value)
+        return
+    }
+})
